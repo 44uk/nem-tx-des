279ffe7c8a40ffc1df002ff8c765b5dda23575fc
Yoshiyuki Ieyama <yukku0423@gmail.com>
2018-04-23 10:40:02 +0900

    update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', function () {
     },
     filters: {
       uxem2xem: function (uxem) {
-        return uxem ? uxem / 1000000 : ''
+        return Number.isInteger(uxem) ? uxem / 1000000 : ''
       },
       nemtime2time: function (nemtime) {
         return nemtime ?
@@ -32,11 +32,14 @@ window.addEventListener('DOMContentLoaded', function () {
       deserialized: function () {
         if (this.serialized == '' || this.serialized == null) { return {} }
         try {
+          let result = {}
           const json = JSON.parse(this.serialized);
           const parsed = nemTxDes.parse(json['data'])
           this.signature = json['signature']
           this.message = null
-          return parsed
+          result = parsed['otherTrans'] ? parsed['otherTrans'] : parsed
+          result['multisig'] = parsed['otherTrans'] ? parsed : null
+          return result
         } catch(err) {
           console.error(err.message)
           this.message = err.message
